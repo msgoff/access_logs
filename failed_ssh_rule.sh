24b795c64127d79654ca60f64ce72fd8cfcbe66b
Michael Goff <msgoff@users.noreply.github.com>
2018-09-14 17:55:05 +0000

    Delete failed_ssh_rule.sh

--- failed_ssh_rule.sh
+++
@@ -1,3 +0,0 @@
-while read f;
-  do echo iptables -I INPUT -s  $f -j DROP ;
-  done < <(cat auth.log |grep ssh|grep fail|tr " " "\n"|grep rhost|sed -re 's/.*?=//g'|sort |uniq -c|sort -n|awk '$1 > 10'|tr " " "\n"|grep "\.")
